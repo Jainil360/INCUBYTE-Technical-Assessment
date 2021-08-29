3144e90e25674b9af02c14dc4eaab317fe869604
Jainil Patel <jainil.tech@gmail.com>
2021-08-29 22:05:55 +0530

    Test 5: Writing Test | Handle new line(delimiter) between number's string

--- tdd_kata_string_calculator/test_tdd_string_calculator.py
+++ tdd_kata_string_calculator/test_tdd_string_calculator.py
@@ -14,3 +14,7 @@ def test_two_numbers_string():
 def test_multiple_numbers_string():
     assert calc.add('1,2,3') == 6
     assert calc.add('100,200,300,50,10,40') == 700
+
+def test_new_line_between_numbers_string():
+    assert calc.add('1\n2,3') == 6
+    assert calc.add('10\n20\n20\n10,20\n20') == 100

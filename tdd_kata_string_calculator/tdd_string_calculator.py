72f72649861588f81bab31de4b8c6cdac318ebd1
Jainil Patel <jainil.tech@gmail.com>
2021-08-29 22:11:11 +0530

    Test 6: Writing code to pass the test | Handle custom delimiters

--- tdd_kata_string_calculator/tdd_string_calculator.py
+++ tdd_kata_string_calculator/tdd_string_calculator.py
@@ -1,6 +1,20 @@
 def add(numbers):
     if numbers == '':return 0
     else:
+        numbers = handle_custom_delimiter(numbers)
         numbers = numbers.replace('\n',',')
         numbers = map(int, numbers.split(','))
     return sum(numbers)
+
+def handle_custom_delimiter(numbers):
+    if numbers.startswith('//'):
+        delimiterDetail,numberString = numbers.split('\n',1)
+        customDelimiter = delimiterDetail[2]
+        delimitersList = [char for char in customDelimiter]
+        if (len(set(delimitersList)) == 1 ):
+            numbers = numberString.replace(customDelimiter,',')
+        else:
+            for delimiter in delimitersList:
+                numberString = numberString.replace(delimiter,',') 
+            numbers = numberString
+    return numbers

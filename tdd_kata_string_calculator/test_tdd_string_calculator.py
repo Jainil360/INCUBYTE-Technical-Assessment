2bdb176c71823c50190cc8be88fb80a6dc45591d
Jainil Patel <jainil.tech@gmail.com>
2021-08-29 21:55:23 +0530

    Test 3: Writing Test | Add 2 numbers string

--- tdd_kata_string_calculator/test_tdd_string_calculator.py
+++ tdd_kata_string_calculator/test_tdd_string_calculator.py
@@ -6,3 +6,7 @@ def test_empty_string():
 def test_single_number_string():
     assert calc.add('6') == 6
     assert calc.add('0') == 0
+
+def test_two_numbers_string():
+    assert calc.add('1,4') == 5
+    assert calc.add('21,21') == 42

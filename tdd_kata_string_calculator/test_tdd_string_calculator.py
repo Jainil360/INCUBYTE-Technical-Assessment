a7b6795a1e8c21c3015f96f97093c86e4d39740c
Jainil Patel <jainil.tech@gmail.com>
2021-08-29 21:48:22 +0530

    Test 2: Return numeric value if single number string | Again writing a test which will fail

--- tdd_kata_string_calculator/test_tdd_string_calculator.py
+++ tdd_kata_string_calculator/test_tdd_string_calculator.py
@@ -2,3 +2,7 @@ import tdd_string_calculator as calc
 
 def test_empty_string():
     assert calc.add('') == 0
+
+def test_single_number_string():
+    assert calc.add('6') == 6
+    assert calc.add('0') == 0

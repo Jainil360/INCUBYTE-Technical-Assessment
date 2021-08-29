7a2aa2ac4c8d4170b499f2108daf985284cf9a3c
Jainil Patel <jainil.tech@gmail.com>
2021-08-29 22:34:27 +0530

    [New File Added] - Writing Optimized code using regex to handle all test cases

---
+++ tdd_kata_string_calculator_method_2_[Optimized]/tdd_string_calculator_optimized.py
@@ -0,0 +1,16 @@
+import re
+callCount = 0
+def add(numbers):
+    global callCount 
+    callCount += 1
+    if numbers == '':return 0
+    else:
+        numbers = map(int, re.findall(r"-?\d+", numbers))
+        numbers = list(filter(lambda x: x < 1000, numbers))
+        negative_numbers = list(filter(lambda x: x < 0, numbers))
+        if negative_numbers: raise Exception('negatives not allowed ' + str(negative_numbers))
+        return sum(numbers)
+ 
+def GetCalledCount():
+    global callCount
+    return callCount

02eba5d604abd568ca81c918b55759854879f9dd
Jainil Patel <jainil.tech@gmail.com>
2021-08-29 22:02:32 +0530

    Test 4: Writing Test | Add unknown amount of numbers in a string

--- tdd_kata_string_calculator/test_tdd_string_calculator.py
+++ tdd_kata_string_calculator/test_tdd_string_calculator.py
@@ -10,3 +10,7 @@ def test_single_number_string():
 def test_two_numbers_string():
     assert calc.add('1,4') == 5
     assert calc.add('21,21') == 42
+
+def test_multiple_numbers_string():
+    assert calc.add('1,2,3') == 6
+    assert calc.add('100,200,300,50,10,40') == 700
